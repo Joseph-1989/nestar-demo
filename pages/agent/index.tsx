84476bfcf9617c19d46ec4b1063e580cf188455d
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-06-12 19:42:13 +0900

    feat: develop mobile version by integrating  useDeviceDetect hook

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,16 +1,22 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
 const AgentList: NextPage = () => {
+  const device = useDeviceDetect();
   const [title, setTitle] = useState<string>("hello");
 
-  return (
-    <div style={{ margin: "20px 0" }}>
-      <Stack className="container">AGENTS</Stack>
-    </div>
-  );
+  if (device === "mobile") {
+    return <Stack>AGENTS MOBILE</Stack>;
+  } else {
+    return (
+      <div style={{ margin: "20px 0" }}>
+        <Stack className="container">AGENTS</Stack>
+      </div>
+    );
+  }
 };
 
 export default withLayoutBasic(AgentList);
--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,10 +1,16 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const PropertyDetail: NextPage = () => {
-  console.log("DETAIL COMPONENT - PAGES ROUTER");
-  return <Container>PROPERTY DETAIL</Container>;
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>PROPERTY DETAIL MOBILE</Stack>;
+  } else {
+    return <Container>PROPERTY DETAIL</Container>;
+  }
 };
 
 export default withLayoutBasic(PropertyDetail);
